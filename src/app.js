78f4e3c313ba81f13d7474556035f21f1b0b2ca6
huangjiahan66 <1198434221@qq.com>
2023-02-14 23:57:52 +0800

    h5-server

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const jsonServer = require('json-server')
+const router = require('./router')
+const db = require('./db')()
+
+const server = jsonServer.create()
+
+const middlewares = jsonServer.defaults({
+    static: path.join(__dirname, '../public')
+})
+server.use(middlewares)
+// req.body
+server.use(jsonServer.bodyParser)
+
+server.use((req, res, next) => {
+    const json = res.json.bind(res)
+    res.success = (data) => {
+        return json({
+            code: 0,
+            msg: '请求成功',
+            data
+        })
+    }
+    res.fail = (msg, code = -1, data) => {
+        return json({
+            code,
+            msg,
+            data
+        })
+    }
+    next()
+})
+
+router(server)
+const jsonRouter = jsonServer.router(db)
+server.use((req, res, next) => {
+    setTimeout(next, 1000)
+})
+server.use('/api', jsonRouter)
+
+server.listen(8000, () => {
+    console.log('JSON Server is running at 8000')
+})
---
+++ src/db.js
@@ -0,0 +1,17 @@
+const test = require('../data/test')
+const homePage = require('../data/home_page')
+
+function responseData(data) {
+    return {
+        code: 0,
+        msg: '请求成功',
+        data,
+    }
+}
+
+module.exports = () => {
+    return {
+        testjson0: test(),
+        home_page: responseData(homePage())
+    }
+}
